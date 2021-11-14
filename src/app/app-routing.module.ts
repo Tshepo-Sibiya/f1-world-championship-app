2d163778242da0549e10bd504a250b19b7062798
Tshepo Sibiya <Tshepo.sibiya@gmail.com>
2021-11-14 16:21:44 +0200

    removed main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RaceWinnersComponent } from './f1-championship-module/components/race-winners/race-winners.component';
-import { SeasonListComponent } from './f1-championship-module/components/season-list/season-list.component';
 
 const routes: Routes = [
-  { path: 'race-winners', component: RaceWinnersComponent },
-  { path: 'seasons', component: SeasonListComponent },
+  // { path: 'race-winners', component: RaceWinnersComponent },
+  // { path: 'seasons', component: SeasonListComponent },
 ];
 
 @NgModule({
